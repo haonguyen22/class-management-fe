c9cd4fb16a7d2d7657d5ff0bb7dde600d63f359c
Hao Nguyen <haonguyencp22@gmail.com>
2024-01-02 01:45:16 +0700

    feat: add detail class

--- src/components/ClassSettings/ClassDetailBox.tsx
+++ src/components/ClassSettings/ClassDetailBox.tsx
@@ -1,17 +1,41 @@
 import { TextField } from '@mui/material';
 import SettingFrameLayout from './SettingFrameLayout';
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+import { IClass } from '../../models/IClass';
+import { apiCall } from '../../utils/apiCall';
+import { classService } from '../../services/class/ClassService';
+import { useParams } from 'react-router-dom';
 
 function ClassDetailBox() {
+  const { id } = useParams<{ id: string }>();
+
   const { t } = useTranslation();
+  const [data, setData] = useState<IClass>();
+
+  const getClassDetail = async () => {
+    await apiCall(classService.getClassInfo(id!), {
+      ifSuccess: (data) => {
+        setData(data.metadata as IClass);
+      },
+      ifFailed: () => {},
+    });
+  };
+
+  useEffect(() => {
+    getClassDetail();
+  }, []);
+
   return (
-    <SettingFrameLayout title="Class Details"  >
+    <SettingFrameLayout title={t('classDetail')}>
       <TextField
         id="filled-basic"
         label={t('className')}
         variant="filled"
         required
+        disabled
         name="name"
+        value={data?.name ?? ''}
         sx={{ padding: '0px', margin: '8px' }}
       />
       <TextField
@@ -19,15 +43,19 @@ function ClassDetailBox() {
         label={t('description')}
         variant="filled"
         required
-        name="name"
+        disabled
+        name="description"
+        value={data?.description ?? ''}
         sx={{ padding: '0px', margin: '8px' }}
       />
       <TextField
         id="filled-basic"
-        label={t('subject')}
+        label={t('owner')}
         variant="filled"
         required
-        name="name"
+        disabled
+        name="owner"
+        value={data?.owner?.email ?? ''}
         sx={{ padding: '0px', margin: '8px' }}
       />
     </SettingFrameLayout>
