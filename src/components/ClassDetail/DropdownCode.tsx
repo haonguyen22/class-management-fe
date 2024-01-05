d7566f6c51a9620b06423e11c6329f61d76f2c16
Hao Nguyen <haonguyencp22@gmail.com>
2024-01-05 23:49:47 +0700

    chore: update link join code

--- src/components/ClassDetail/DropdownCode.tsx
+++ src/components/ClassDetail/DropdownCode.tsx
@@ -65,11 +65,7 @@ const DropdownCode: React.FC<DropdownCodeProps> = ({ code, type }) => {
         <MenuItem
           onClick={() =>
             handleCopy(
-              `${
-                process.env?.REACT_APP_CLIENT_URL !== '' ?? false
-                  ? process.env?.REACT_APP_CLIENT_URL
-                  : `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`
-              }/class/join?code=${code}`,
+              `https://class-management-fe.vercel.app/class/join?code=${code}`,
             )
           }
         >
